89c12bc3dfeaf0873c2e0282c69b0434574b46a3
Giga <60932604+Gkhundadze@users.noreply.github.com>
2021-05-04 23:08:31 +0400

    added theme switcher

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 let burger = document.querySelector('.burger-button');
 let burgerTwo = document.querySelector('.burger-button-second');
+let themeSwitcher = document.querySelector('#checkbox');
+let switchedLabel = document.querySelector('.chackbox-label')
 burger.addEventListener('click', () => {
  burger.classList.toggle('open')
     
 })
 burgerTwo.addEventListener('click', () => {
     burgerTwo.classList.toggle('clicked')
+})
+themeSwitcher.addEventListener('change', ()=> {
+    switchedLabel.classList.toggle('checked')
+    document.body.classList.toggle('dark')
 })
